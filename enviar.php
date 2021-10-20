ac722d50373c2b7a766d541ab3ba19a1d85a7c71
Santiago Güerci <santiago.38@hotmail.com>
2021-10-20 18:24:07 -0300

    primer commit

---
+++ enviar.php
@@ -0,0 +1,28 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$direccion = $_POST['direccion'];
+$email = $_POST['email'];
+$ciudad = $_POST['ciudad'];
+$provincia = $_POST['provincia'];
+$codigoPostal = $_POST['codigoPostal'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Es de: " . $provincia . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'santiago.3861@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:contacto.html');
+echo "Thank You!";
+?>
---
+++ main.js
@@ -0,0 +1,25 @@
+//Simulador carrito de compras de AxelCocina
+
+// 1)Armar caja: array con objetos "producto" {producto, cantidad y precio}
+//   Y luego un array de arrays (es decir, un pedido con varias cajas).
+
+//=============== Variables y Funciones ===================
+
+
+
+
+
+
+
+//=============== Correr el programa ===================
+alert("Bienvenido a AxelCocina - Panadería y Pastelería");
+alert("Aquí podrá armar sus cajas a gusto, y luego proceder a comprarlas.");
+
+alert("Ingrese un producto a incluir en la caja: \n
+       1: Pan de Masa Madre \n
+       2: Macaron \n
+       3: Mermelada casera \n
+       4: Minitorta Brownie \n
+       5: Minitorta Cheesecake \n
+       6: Minitorta Lemon Pie \n
+       7: Chipa");
